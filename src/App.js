a3a1e96ab3795804d8f43d19771018b9c48727e7
Luciana Corti <lulicorti19@gmail.com>
2022-10-04 17:21:00 -0300

    agregue contador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Counter from "./components/Counter/Counter";
 
 function App() {
+  const handleOnAdd = () => {
+    console.log("agregaste al carrito");
+  };
+
   return (
     <div className="App">
       <Navbar />
       <ItemListContainer gretting={"Bienvenidos a nuestra tienda"} />
+      <Counter onAdd={handleOnAdd} />
     </div>
   );
 }
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./Counter.css";
+
+let stock = 10;
+
+const Counter = ({ onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="divCounter">
+      <h1 className="tituloContador">Contador</h1>
+      <div className="divCounter1">
+        <button
+          type="button"
+          className="btn btn-light p-2 m-3 btn-outline-dark"
+          href="#"
+          onClick={increment}
+        >
+          +
+        </button>
+        <h2 className="numContador">{count}</h2>
+        <button
+          type="button"
+          className="btn btn-light p-2 m-3 btn-outline-dark"
+          href="#"
+          onClick={decrement}
+        >
+          -
+        </button>
+      </div>
+      <button
+        type="button"
+        className="btn btn-light p-2 m-3 btn-outline-dark"
+        href="#"
+        onClick={onAdd}
+      >
+        Agregar al carrito
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
